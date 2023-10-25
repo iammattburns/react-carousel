8aa28a2307da080c5915cd4ab4eb5605c5dd2e1a
Matt Burns <iammattburns@gmail.com>
2023-10-25 22:58:38 +0100

    Initial commit

---
+++ rollup.config.js
@@ -0,0 +1,26 @@
+import babel from '@rollup/plugin-babel'
+import resolve from '@rollup/plugin-node-resolve'
+import commonjs from '@rollup/plugin-commonjs'
+import terser from '@rollup/plugin-terser'
+import typescript from '@rollup/plugin-typescript'
+import pkg from './package.json' assert { type: 'json' }
+
+export default {
+  input: 'src/index.ts',
+  output: [
+    { file: pkg.main, format: 'cjs' },
+    { file: pkg.module, format: 'esm' },
+  ],
+  plugins: [
+    babel({
+      babelHelpers: 'bundled',
+      exclude: 'node_modules/**',
+      presets: ['@babel/preset-env', '@babel/preset-react'],
+    }),
+    resolve(),
+    commonjs(),
+    terser(),
+    typescript(),
+  ],
+  external: Object.keys(pkg.peerDependencies),
+}
