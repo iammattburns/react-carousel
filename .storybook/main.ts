b304283d2e612c0380e45870c5b928e15cc17968
Matt Burns <iammattburns@gmail.com>
2023-10-26 10:30:38 +0100

    build: adds storybook addons

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,12 +1,20 @@
-module.exports = {
+import type { StorybookConfig } from '@storybook/react-webpack5'
+const config: StorybookConfig = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
-
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+    '@storybook/addon-a11y',
+    'storybook-addon-pseudo-states',
+    '@storybook/addon-docs',
+  ],
   framework: {
     name: '@storybook/react-webpack5',
     options: {},
   },
-
   docs: {
-    autodocs: false,
+    autodocs: true,
   },
 }
+export default config
