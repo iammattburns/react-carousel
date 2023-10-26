ababc196f08fde943d46b82bfcab5a89eeea4b57
Matt Burns <iammattburns@gmail.com>
2023-10-26 10:32:01 +0100

    chore: remove demo button component

--- src/components/Button/Button.stories.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import Button from "./Button";
-
-export default {
-  component: Button,
-  title: "Button",
-};
-
-export const Default = () => <Button label="Click me!" />;
